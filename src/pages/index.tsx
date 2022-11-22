ef4b190949e16ea0f1360c66610c9b006364a788
dkme01 <dkme01@gmail.com>
2022-11-22 18:14:18 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,8 +60,6 @@ export default function Home() {
   const getModelInfo = async () => {
     const materialsFromModel = await p3d.current?.listMaterials();
 
-    setLoading(true);
-
     console.log("getModelInfo", materialsFromModel);
 
     if (materialsFromModel.length > 0) {
@@ -69,12 +67,10 @@ export default function Home() {
       setMaterialsList(materialsFromModel);
       setColors(() => materials.current.map((material: any) => ({ hex: material.baseColor })));
       setGlowColor(() => materials.current.map((material: any) => ({ hex: material.glowColor })));
-      setLoading(false);
     }
   };
 
   useEffect(() => {
-    // if (new3DModel.length > 4) {
     p3d.current = new P3dEmbedApi(document.getElementById("p3d-embed"), {
       onready: getModelInfo,
     });
@@ -82,7 +78,6 @@ export default function Home() {
     p3d.current?.setAllowCameraReset("if-outside");
 
     p3d.current?.setAllowCameraRecenter(false);
-    // }
   }, [p3d, new3DModel]);
 
   const handleChangeColor = (color: any, colorIndex: number) => {
@@ -137,170 +132,156 @@ export default function Home() {
   };
 
   return (
-    <>
-      <Container>
-        <AppBar component="nav">
-          <Toolbar>
-            <IconButton color="inherit" aria-label="open drawer" edge="start" sx={{ mr: 2, display: { sm: "none" } }}>
-              <Menu />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
-              3D Prototype
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Box marginTop="8vh" marginBottom="1vh">
-          <TextField onChange={(e) => handleChange3DValue(e)} value={new3DModel} label="Link do modelo 3D" />
-        </Box>
-        <Grid spacing={2} container display="flex" direction="row">
-          <Grid container item xs={12} md={7}>
-            <Grid item xs={12} sx={{ maxHeight: "70vh", height: "70vh" }}>
-              {loading ? (
-                <CircularProgress />
-              ) : (
-                <iframe
-                  frameBorder="0"
-                  src={`https://p3d.in/e/${
-                    new3DModel.length > 0 ? new3DModel : "Dj8Js"
-                  }+api+load+controls,loader-hidden`}
-                  id="p3d-embed"
-                ></iframe>
-              )}
-            </Grid>
+    <Container>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton color="inherit" aria-label="open drawer" edge="start" sx={{ mr: 2, display: { sm: "none" } }}>
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}>
+            3D Prototype
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box marginTop="8vh" marginBottom="1vh">
+        <TextField onChange={(e) => handleChange3DValue(e)} value={new3DModel} label="Link do modelo 3D" />
+      </Box>
+      <Grid spacing={2} container display="flex" direction="row">
+        <Grid container item xs={12} md={7}>
+          <Grid item xs={12} sx={{ maxHeight: "70vh", height: "70vh" }}>
+            <iframe
+              frameBorder="0"
+              src={`https://p3d.in/e/${new3DModel.length > 0 ? new3DModel : "Dj8Js"}+api+load+controls,loader-hidden`}
+              id="p3d-embed"
+            ></iframe>
           </Grid>
-          <Box sx={{ width: viewPort ? "25vw" : "100%", height: viewPort ? "75vh" : "100%", overflow: "auto" }}>
-            {materialsList.length > 0 ? (
-              materialsList?.map((material: any, index: number) => (
-                <Box margin={1} key={`${material.name}_${index + 1}`}>
-                  <Accordion expanded={panelExpanded === index} onChange={() => onChangePanelOpen(index)}>
-                    <AccordionSummary expandIcon={<ExpandMore />}>
-                      <Typography>{`${index + 1}: ${material.name}`}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
+        </Grid>
+        <Box sx={{ width: viewPort ? "25vw" : "100%", height: viewPort ? "75vh" : "100%", overflow: "auto" }}>
+          {materialsList.length > 0 ? (
+            materialsList?.map((material: any, index: number) => (
+              <Box margin={1} key={`${material.name}_${index + 1}`}>
+                <Accordion expanded={panelExpanded === index} onChange={() => onChangePanelOpen(index)}>
+                  <AccordionSummary expandIcon={<ExpandMore />}>
+                    <Typography>{`${index + 1}: ${material.name}`}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Box>
                       <Box>
-                        <Box>
-                          <Tooltip title="Cor base do material">
-                            <Typography sx={{ mb: 1 }} variant="body2">
-                              Base color
-                            </Typography>
-                          </Tooltip>
-                          <MaterialPicker
-                            color={colors[index]}
-                            onChange={(color) => handleChangeColor(color, index)}
-                            disableAlpha
-                          />
-                          <Box sx={{ mt: 2 }}>
-                            <Tooltip title="Fator da cor base">
-                              <Typography variant="body2">Base color factor</Typography>
-                            </Tooltip>
-                            <Slider
-                              valueLabelFormat={(label) => label / 1000}
-                              valueLabelDisplay="auto"
-                              max={1000}
-                              onChange={(_, value) =>
-                                handleChangeMaterialValue(Number(value), "baseColorFactor", index)
-                              }
-                              value={material.baseColorFactor * 1000}
-                            />
-                          </Box>
-                        </Box>
-                        <Box>
-                          <Tooltip title="Acabamento metálico do material">
-                            <Typography variant="body2">Metalness</Typography>
+                        <Tooltip title="Cor base do material">
+                          <Typography sx={{ mb: 1 }} variant="body2">
+                            Base color
+                          </Typography>
+                        </Tooltip>
+                        <MaterialPicker
+                          color={colors[index]}
+                          onChange={(color) => handleChangeColor(color, index)}
+                          disableAlpha
+                        />
+                        <Box sx={{ mt: 2 }}>
+                          <Tooltip title="Fator da cor base">
+                            <Typography variant="body2">Base color factor</Typography>
                           </Tooltip>
                           <Slider
                             valueLabelFormat={(label) => label / 1000}
                             valueLabelDisplay="auto"
                             max={1000}
-                            onChange={(_, value) => handleChangeMaterialValue(Number(value), "metalness", index)}
-                            value={material.metalness * 1000}
+                            onChange={(_, value) => handleChangeMaterialValue(Number(value), "baseColorFactor", index)}
+                            value={material.baseColorFactor * 1000}
                           />
-                        </Box>
-                        <Box>
-                          <Tooltip title="Aspereza do material">
-                            <Typography variant="body2">Roughness</Typography>
-                          </Tooltip>
-                          <Slider
-                            valueLabelFormat={(label) => label / 1000}
-                            valueLabelDisplay="auto"
-                            max={1000}
-                            onChange={(_, value) => handleChangeMaterialValue(Number(value), "roughness", index)}
-                            value={material.roughness * 1000}
-                          />
-                        </Box>
-                        <Box>
-                          <Tooltip title="Reflexo especular em volta do material">
-                            <Typography variant="body2">Sheen</Typography>
-                          </Tooltip>
-                          <Slider
-                            valueLabelFormat={(label) => label / 1000}
-                            valueLabelDisplay="auto"
-                            max={1000}
-                            onChange={(_, value) => handleChangeMaterialValue(Number(value), "sheenFactor", index)}
-                            value={material.sheenFactor * 1000}
-                          />
-                        </Box>
-                        <Box>
-                          <Tooltip title="Camada de acabamento (ex: verniz)">
-                            <Typography variant="body2">Clearcoat</Typography>
-                          </Tooltip>
-                          <Slider
-                            valueLabelFormat={(label) => label / 1000}
-                            valueLabelDisplay="auto"
-                            max={1000}
-                            onChange={(_, value) => handleChangeMaterialValue(Number(value), "clearCoat", index)}
-                            value={material.clearCoat * 1000}
-                          />
-                        </Box>
-                        <Box>
-                          <Tooltip title="Aspereza da camada de acabamento">
-                            <Typography variant="body2">Clearcoat Roughness</Typography>
-                          </Tooltip>
-                          <Slider
-                            valueLabelFormat={(label) => label / 1000}
-                            valueLabelDisplay="auto"
-                            max={1000}
-                            onChange={(_, value) =>
-                              handleChangeMaterialValue(Number(value), "clearCoatRoughness", index)
-                            }
-                            value={material.clearCoatRoughness * 1000}
-                          />
-                        </Box>
-                        <Box>
-                          <Tooltip title="Capacidade de luminosa do material">
-                            <Typography variant="body2">Emissivo</Typography>
-                          </Tooltip>
-                          <MaterialPicker
-                            color={glowColor[index]}
-                            onChange={(color) => handleChangeGlow(color, index)}
-                            disableAlpha
-                          />
-                          <Box sx={{ mt: 2 }}>
-                            <Tooltip title="Fator da emissividade do material">
-                              <Typography variant="body2">Fator emissivo</Typography>
-                            </Tooltip>
-                            <Slider
-                              valueLabelFormat={(label) => label / 1000}
-                              valueLabelDisplay="auto"
-                              max={1000}
-                              onChange={(_, value) => handleChangeMaterialValue(Number(value), "glowFactor", index)}
-                              value={material.glowFactor * 1000}
-                            />
-                          </Box>
                         </Box>
                       </Box>
-                    </AccordionDetails>
-                  </Accordion>
-                </Box>
-              ))
-            ) : (
-              <></>
-            )}
-          </Box>
-        </Grid>
-      </Container>
-      {/* <Script src="https://cfstatic.p3d.in/embed-api/v2/p3d-embed-api.js" />
-      <Script src="https://unpkg.com/@tweenjs/tween.js@18.6.4/dist/tween.umd.js" /> */}
-    </>
+                      <Box>
+                        <Tooltip title="Acabamento metálico do material">
+                          <Typography variant="body2">Metalness</Typography>
+                        </Tooltip>
+                        <Slider
+                          valueLabelFormat={(label) => label / 1000}
+                          valueLabelDisplay="auto"
+                          max={1000}
+                          onChange={(_, value) => handleChangeMaterialValue(Number(value), "metalness", index)}
+                          value={material.metalness * 1000}
+                        />
+                      </Box>
+                      <Box>
+                        <Tooltip title="Aspereza do material">
+                          <Typography variant="body2">Roughness</Typography>
+                        </Tooltip>
+                        <Slider
+                          valueLabelFormat={(label) => label / 1000}
+                          valueLabelDisplay="auto"
+                          max={1000}
+                          onChange={(_, value) => handleChangeMaterialValue(Number(value), "roughness", index)}
+                          value={material.roughness * 1000}
+                        />
+                      </Box>
+                      <Box>
+                        <Tooltip title="Reflexo especular em volta do material">
+                          <Typography variant="body2">Sheen</Typography>
+                        </Tooltip>
+                        <Slider
+                          valueLabelFormat={(label) => label / 1000}
+                          valueLabelDisplay="auto"
+                          max={1000}
+                          onChange={(_, value) => handleChangeMaterialValue(Number(value), "sheenFactor", index)}
+                          value={material.sheenFactor * 1000}
+                        />
+                      </Box>
+                      <Box>
+                        <Tooltip title="Camada de acabamento (ex: verniz)">
+                          <Typography variant="body2">Clearcoat</Typography>
+                        </Tooltip>
+                        <Slider
+                          valueLabelFormat={(label) => label / 1000}
+                          valueLabelDisplay="auto"
+                          max={1000}
+                          onChange={(_, value) => handleChangeMaterialValue(Number(value), "clearCoat", index)}
+                          value={material.clearCoat * 1000}
+                        />
+                      </Box>
+                      <Box>
+                        <Tooltip title="Aspereza da camada de acabamento">
+                          <Typography variant="body2">Clearcoat Roughness</Typography>
+                        </Tooltip>
+                        <Slider
+                          valueLabelFormat={(label) => label / 1000}
+                          valueLabelDisplay="auto"
+                          max={1000}
+                          onChange={(_, value) => handleChangeMaterialValue(Number(value), "clearCoatRoughness", index)}
+                          value={material.clearCoatRoughness * 1000}
+                        />
+                      </Box>
+                      <Box>
+                        <Tooltip title="Capacidade de luminosa do material">
+                          <Typography variant="body2">Emissivo</Typography>
+                        </Tooltip>
+                        <MaterialPicker
+                          color={glowColor[index]}
+                          onChange={(color) => handleChangeGlow(color, index)}
+                          disableAlpha
+                        />
+                        <Box sx={{ mt: 2 }}>
+                          <Tooltip title="Fator da emissividade do material">
+                            <Typography variant="body2">Fator emissivo</Typography>
+                          </Tooltip>
+                          <Slider
+                            valueLabelFormat={(label) => label / 1000}
+                            valueLabelDisplay="auto"
+                            max={1000}
+                            onChange={(_, value) => handleChangeMaterialValue(Number(value), "glowFactor", index)}
+                            value={material.glowFactor * 1000}
+                          />
+                        </Box>
+                      </Box>
+                    </Box>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            ))
+          ) : (
+            <></>
+          )}
+        </Box>
+      </Grid>
+    </Container>
   );
 }
